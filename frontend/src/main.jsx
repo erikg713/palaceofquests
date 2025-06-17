ae75f438abec46c67cc11ee3b8ecf4f9a7caf5d9
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-17 07:07:35 +0000

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { PiWalletProvider } from './context/PiWalletContext.jsx';
+import { mockPiSDK } from './utils/mockPi.js';
 
+mockPiSDK();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PiWalletProvider>
+      <App />
+    </PiWalletProvider>
+  </React.StrictMode>
+);
