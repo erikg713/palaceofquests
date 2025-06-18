e2e674bfdbdb197f562648f5be1f75cfd1ac5ab8
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-18 03:03:31 +0000

    Update db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,1 +1,11 @@
+from supabase import create_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_ANON_KEY")
+
+supabase = create_client(supabase_url, supabase_key)
 
