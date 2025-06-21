5170f8fd5f8a00a53912d5d77af2ec97dbc0029f
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-21 00:58:01 -0500

    Update config.py

--- backend/flask_app/config.py
+++ backend/flask_app/config.py
@@ -1,7 +1,1 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///dev.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
