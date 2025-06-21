ece8d42af9c7d9e0651d8f75550b5756bb6de6be
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-21 13:34:23 -0500

    Update MarketplaceItem.jsx

--- frontend/src/components/MarketplaceItem.jsx
+++ frontend/src/components/MarketplaceItem.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+
+export default function MarketplaceItem({ item, onBuy }) {
+  const handleBuy = () => {
+    if (onBuy) onBuy(item);
+  };
+
+  return (
+    <div className={`market-item rarity-${item.rarity.toLowerCase()}`}>
+      <img src={item.iconUrl} alt={item.name} className="item-img" />
+      <div className="item-info">
+        <div className="item-name">{item.name}</div>
+        <div className="item-type">{item.type}</div>
+        <div className="item-price">{item.price} Pi</div>
+      </div>
+      <button className="buy-button" onClick={handleBuy}>
+        Buy
+      </button>
+    </div>
+  );
+}
 
