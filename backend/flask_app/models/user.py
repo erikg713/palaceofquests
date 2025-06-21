a3e5db139106ba997ce9c3db24da8122c55ab660
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-20 22:51:09 -0500

    Update user.py

--- backend/flask_app/models/user.py
+++ backend/flask_app/models/user.py
@@ -1,1 +1,6 @@
+from .. import db
+from uuid import uuid4
 
+class User(db.Model):
+    id = db.Column(db.String, primary_key=True, default=lambda: str(uuid4()))
+    pi_username = db.Column(db.String, unique=True, nullable=False)
