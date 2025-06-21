14af612ad8ba708a1a7a0e9b321d4ed4a990628c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-21 00:11:23 -0500

    Update inventoryCard.jsx

--- frontend/src/components/ui/inventoryCard.jsx
+++ frontend/src/components/ui/inventoryCard.jsx
@@ -8,15 +8,3 @@ function ItemCard({ item, onClick }) {
       <div className="item-name">{item.name}</div>
     </div>
   )
-}
-function ItemCard({ item, onClick }) {
-  return (
-    <div className={`item-card rarity-${item.rarity.toLowerCase()}`} onClick={() => onClick(item)}>
-      <img src={item.iconUrl} alt={item.name} />
-      <div className="item-stars">
-        {'★'.repeat(item.stars)}
-      </div>
-      <div className="item-name">{item.name}</div>
-    </div>
-  )
-}
