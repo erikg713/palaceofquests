6eb149809720abcf2fd463818f3a9fc88972cec4
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-21 00:01:30 -0500

    Create inventoryCard.jsx

---
+++ frontend/src/components/ui/inventoryCard.jsx
@@ -0,0 +1,11 @@
+function ItemCard({ item, onClick }) {
+  return (
+    <div className={`item-card rarity-${item.rarity.toLowerCase()}`} onClick={() => onClick(item)}>
+      <img src={item.iconUrl} alt={item.name} />
+      <div className="item-stars">
+        {'★'.repeat(item.stars)}
+      </div>
+      <div className="item-name">{item.name}</div>
+    </div>
+  )
+}
