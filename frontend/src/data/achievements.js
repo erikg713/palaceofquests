263bbc1fe1dd11a215af9381dece7b2c0ea4e72d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-23 07:22:18 +0000

    Create achievements.js

---
+++ frontend/src/data/achievements.js
@@ -0,0 +1,26 @@
+export const allAchievements = [
+  {
+    id: 'first_quest',
+    name: 'First Blood',
+    description: 'Complete your very first quest.',
+    icon: '/badges/first_quest.png'
+  },
+  {
+    id: 'pi_spender',
+    name: 'Pi Spender',
+    description: 'Spend Pi tokens on any upgrade.',
+    icon: '/badges/pi_spender.png'
+  },
+  {
+    id: 'moon_unlock',
+    name: 'Moon Explorer',
+    description: 'Unlock the Moon Fortress realm.',
+    icon: '/badges/moon_unlock.png'
+  },
+  {
+    id: 'fusion_master',
+    name: 'Fusion Master',
+    description: 'Fuse two gear items together.',
+    icon: '/badges/fusion_master.png'
+  }
+];
