b1fb7bc3c25ae828cc58baf87737ed92c0623d72
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-23 07:15:31 +0000

    Create realms.js

---
+++ frontend/src/data/realms.js
@@ -0,0 +1,18 @@
+export const realms = [
+  {
+    id: 'moon_fortress',
+    name: 'Moon Fortress',
+    unlockCost: 2,
+    description: 'A defensive stronghold orbiting Earth.',
+    coordinates: [200, 150],
+    image: '/maps/moon.png'
+  },
+  {
+    id: 'plasma_citadel',
+    name: 'Plasma Citadel',
+    unlockCost: 3,
+    description: 'A volcanic realm crackling with energy.',
+    coordinates: [400, 300],
+    image: '/maps/plasma.png'
+  }
+];
