92c8ea65f3c9e76ef9a650a732f89a0042767da9
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-26 02:07:55 -0500

    Update market_items.sql

--- database/tables/market_items.sql
+++ database/tables/market_items.sql
@@ -5,3 +5,9 @@ CREATE TABLE market_items (
   rarity text,
   realm_id text
 );
+
+INSERT INTO market_items (item_name, price, rarity, realm_id)
+VALUES
+('Quantum Axe', 5, 'rare', 'market'),
+('Photon Cloak', 7, 'epic', 'market'),
+('Synth Blade', 3, 'uncommon', 'market');
