b1d1e8457fb59531f95ac1f3a163697caf89b895
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-06-29 00:20:56 -0500

    Update usePiAuth.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/hooks/usePiAuth.js
+++ frontend/src/hooks/usePiAuth.js
@@ -25,3 +25,30 @@ export async function loginWithPi() {
     }
   });
 }
+export async function loginWithPi() {
+  const scopes = ['username', 'payments'];
+  return new Promise((resolve, reject) => {
+    window.Pi.authenticate(scopes, onSuccess, onFailure);
+
+    function onSuccess(auth) {
+      fetch('/auth/pi-login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(auth),
+      })
+        .then(res => res.json())
+        .then(data => {
+          if (data.token) {
+            localStorage.setItem('token', data.token);
+            resolve(data);
+          } else {
+            reject(data);
+          }
+        });
+    }
+
+    function onFailure(error) {
+      reject(error);
+    }
+  });
+}
