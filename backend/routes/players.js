b627870e8a42bf38c0951f6d220dfc305250eaf4
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-03 22:55:06 -0500

    Update players.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/routes/players.js
+++ backend/routes/players.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const db = require('../db');
+const router = express.Router();
+
+router.get('/:uid', async (req, res) => {
+  const { uid } = req.params;
+  const result = await db.query('SELECT * FROM players WHERE pi_uid = $1', [uid]);
+  res.json(result.rows[0]);
+});
+
+module.exports = router;
 
