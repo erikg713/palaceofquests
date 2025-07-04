cacf6a3b201942e0dbf8b4be420ed9d8fbbebf47
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-04 02:55:31 -0500

    Update Dashboard.jsx
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,7 +5,8 @@ export default function Dashboard({ user }) {
     <div>
       <h1>Welcome, {user.username}</h1>
       <p>Your UID: {user.uid}</p>
+       <p>Your wallet: {user.wallet}</p>
+      <p>Role: {user.role}</p>
     </div>
   );
 }
-
