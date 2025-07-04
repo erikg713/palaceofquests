657e58263afaa60fd3ac15c1aaf74cf126d72721
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-03 22:25:35 -0500

    Delete backend/flask_app directory
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/flask_app/routes/POST/payment/submit_payment.py
+++
@@ -1,5 +0,0 @@
-@auth_bp.route('/payment/submit', methods=['POST'])
-def submit_payment():
-    payment_id = request.json.get('paymentId')
-    txid = pi.submit_payment(payment_id)
-    return jsonify({ "txid": txid })
