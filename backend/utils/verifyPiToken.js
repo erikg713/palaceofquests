b6fde28c6f2a5a01bc08b0f3044178cfae374455
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-04 02:49:51 -0500

    Update verifyPiToken.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/utils/verifyPiToken.js
+++ backend/utils/verifyPiToken.js
@@ -1,1 +1,27 @@
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+
+let cachedPublicKey = null;
+
+async function getPiPublicKey() {
+  if (cachedPublicKey) return cachedPublicKey;
+
+  const res = await axios.get('https://api.minepi.com/pi/users/public_key');
+  cachedPublicKey = res.data;
+  return cachedPublicKey;
+}
+
+module.exports = async function verifyPiToken(accessToken) {
+  const publicKey = await getPiPublicKey();
+
+  try {
+    const decoded = jwt.verify(accessToken, publicKey, {
+      algorithms: ['ES256'],
+    });
+
+    return decoded; // Contains: uid, username, user_wallet_address
+  } catch (err) {
+    throw new Error('Invalid access token');
+  }
+};
 
