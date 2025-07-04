e2d58c1487c27c66557f2a30dd41b911e1043f59
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-03 23:22:28 -0500

    Update vite.config.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
-    https: true,         // ✅ serve over HTTPS!
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true
-  },
-  build: {
-    sourcemap: true
-  },
-  define: {
-    'process.env': process.env
+    port: 3000
   }
 });
