9de067bf6d89cf044f3ed113498e74640df76ae6
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-10 11:50:34 -0500

    Update questController.js
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

--- backend/src/controllers/questController.js
+++ backend/src/controllers/questController.js
@@ -1,1 +1,10 @@
+const { quests } = require('../models/questModel');
 
+exports.getAllQuests = (req, res) => {
+  res.json(quests);
+};
+
+exports.getQuestById = (req, res) => {
+  const quest = quests.find(q => q.id === +req.params.id);
+  quest ? res.json(quest) : res.status(404).json({ error: 'Not found' });
+};
