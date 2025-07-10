af9fef3a9591d8c154d4b017fe4ffcb5578d3204
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-07-10 10:26:47 -0500

    Create FadOutBox.jsx
    Signed-off-by: Dev713 <53387865+erikg713@users.noreply.github.com>

---
+++ frontend/src/components/FadOutBox.jsx
@@ -0,0 +1,7 @@
+export default function FadeOutBox() {
+  return (
+    <div className="w-64 h-32 bg-blue-500 text-white flex items-center justify-center animate-fade">
+      This will fade out
+    </div>
+  );
+}
